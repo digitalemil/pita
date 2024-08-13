70bf3c032f10f63e070beb49a986683741f40c43
Emil A. Siemes <emil@Emils-MacBook-Pro.fritz.box>
2024-08-13 17:37:15 +0200

    instrumentation in start.sh

--- node/start.sh
+++ node/start.sh
@@ -1,4 +1,4 @@
 cd /opt/app
 
 /opt/app/grafana-agent-linux-amd64  -config.expand-env -enable-features integrations-next --config.file /opt/app/grafana-agent-config.yaml &
-node ./bin/www 
+node --require @opentelemetry/auto-instrumentations-node/register ./bin/www 
