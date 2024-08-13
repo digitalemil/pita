8f9ef7e54c0144d28fa2bfe6c3c1c6be55dcae1f
Emil A. Siemes <emil@Emils-MacBook-Pro.fritz.box>
2024-08-13 20:47:39 +0200

    grafana logs

--- node/start.sh
+++ node/start.sh
@@ -1,4 +1,4 @@
 cd /opt/app
 
-/opt/app/grafana-agent-linux-amd64  -config.expand-env -enable-features integrations-next --config.file /opt/app/grafana-agent-config.yaml &
+/opt/app/grafana-agent-linux-amd64  -config.expand-env -enable-features integrations-next --config.file /opt/app/grafana-agent-config.yaml  > $LOGFOLDER/grafana.log 2>&1 &
 node --require @opentelemetry/auto-instrumentations-node/register ./bin/www 
