388ca87ca95c816b37d68cc8d28aa19d1fe51870
Emil A. Siemes <emil@Emils-MacBook-Pro.fritz.box>
2024-08-18 14:11:56 +0200

    nginx

--- node/start.sh
+++ node/start.sh
@@ -1,5 +1,6 @@
 cd /opt/app
 
-/usr/sbin/nginx &
+node --require @opentelemetry/auto-instrumentations-node/register ./bin/www &
 /opt/app/grafana-agent-linux-amd64  -config.expand-env -enable-features integrations-next --config.file /opt/app/grafana-agent-config.yaml  > $LOGFOLDER/grafana.log 2>&1 &
-node --require @opentelemetry/auto-instrumentations-node/register ./bin/www 
+
+/usr/sbin/nginx 
