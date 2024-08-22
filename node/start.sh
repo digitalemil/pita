984134ddd25d05efd44898e49bf148919727450f
Emil A. Siemes <emil@Emils-MBP.fritz.box>
2024-08-22 19:23:04 +0200

    move to bun

--- node/start.sh
+++ node/start.sh
@@ -4,5 +4,6 @@ cd /opt/app
 
 /opt/app/grafana-agent-linux-amd64  -config.expand-env -enable-features integrations-next --config.file /opt/app/grafana-agent-config.yaml  > $LOGFOLDER/grafana.log 2>&1 &
 
-node --require @opentelemetry/auto-instrumentations-node/register ./bin/www 
+bun run --preload @opentelemetry/auto-instrumentations-node/register ./bin/www.js
+#node --require @opentelemetry/auto-instrumentations-node/register ./bin/www 
 
