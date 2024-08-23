41189cfe6ed8c94452d6f043c870ad109ea8f3b2
Emil A. Siemes <emil@Emils-MBP.fritz.box>
2024-08-23 09:26:32 +0200

    no bun compile

--- node/start.sh
+++ node/start.sh
@@ -4,9 +4,9 @@ cd /opt/app
 
 /opt/app/grafana-agent-linux-amd64  -config.expand-env -enable-features integrations-next --config.file /opt/app/grafana-agent-config.yaml  > $LOGFOLDER/grafana.log 2>&1 &
 
-bun build --compile ./bin/www.js --preload @opentelemetry/auto-instrumentations-node/register --outfile pita
-./pita
+#bun build --compile ./bin/www.js --preload @opentelemetry/auto-instrumentations-node/register --outfile pita
+#./pita
 
-#bun run --preload @opentelemetry/auto-instrumentations-node/register ./bin/www.js
+bun run --preload @opentelemetry/auto-instrumentations-node/register ./bin/www.js
 #node --require @opentelemetry/auto-instrumentations-node/register ./bin/www 
 
