58eebd12199819edfe81ddde085b604c6f43b660
Emil A. Siemes <emil@Emils-MBP.fritz.box>
2024-08-24 06:05:43 +0200

    start.sh tail

--- app/start.sh
+++ app/start.sh
@@ -3,6 +3,8 @@ cd /opt/app
 
 bun run --preload @opentelemetry/auto-instrumentations-node/register ./bin/www.js &
 
+sleep 1
+
 /opt/app/grafana-agent-linux-amd64  -config.expand-env -enable-features integrations-next --config.file /opt/app/grafana-agent-config.yaml  > $LOGFOLDER/grafana.log 2>&1 &
 
 /usr/sbin/nginx 
